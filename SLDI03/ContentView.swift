edb5e2f6f05c1f847f1296a0a7c4f6133cbaaa93
Anna <annaersh1994@gmail.com>
2022-04-08 22:07:58 +0300

    Initial Commit

---
+++ SLDI03/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SLDI03
+//
+//  Created by Anna Ershova on 08.04.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SLDI03/SLDI03App.swift
@@ -0,0 +1,17 @@
+//
+//  SLDI03App.swift
+//  SLDI03
+//
+//  Created by Anna Ershova on 08.04.2022.
+//
+
+import SwiftUI
+
+@main
+struct SLDI03App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
